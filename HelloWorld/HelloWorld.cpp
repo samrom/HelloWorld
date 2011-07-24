dc5ce08140f8506e65b30a41b1e3c55b22ec36d5
SamRom <sam.romano18@gmail.com>
2011-07-23 20:24:55 -0700

    Final changes to the project, ship it.

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -8,6 +8,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	printf("Hello World\n");
 	printf("This is saying hello again\n");
+	printf("Now, we are going to test a full source control cycle\n");
 	return 0;
 }
 
